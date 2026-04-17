de32375eebe5fb1b4fc342fb4bcda65c826b7eab
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-04-17 15:55:13 +0300

    [chore] initialise spring boot project structure

---
+++ src/main/java/com/example/blue_lagoon_service/BlueLagoonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.blue_lagoon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueLagoonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueLagoonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blue_lagoon_service/BlueLagoonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blue_lagoon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueLagoonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
