94709467a9959b6d708dc0b5c7b13b24728f596c
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-05-04 19:04:25 +0300

    [fix] add correct be service url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://blue-lagoon-service-be-121043174997.africa-south1.run.app/api/reservations',
+  apiUrl: 'https://blue-lagoon-service-be-121043174997.europe-west1.run.app/api/reservations',
 };
